6713aadcdc560f9c5ffc661fe4b488cf0e708a87
balakumarcse513 <71262331+balakumarcse513@users.noreply.github.com>
2020-09-15 12:57:32 +0530

    Update main.tf

--- TerraformPOC/main.tf
+++ TerraformPOC/main.tf
@@ -6,15 +6,16 @@ provider "azurerm" {
   version         = "=2.0.0"
   features {}
 }
-resource "azurerm_resource_group" "RG" {
-  name     = "TerraformResourceGroup"
-  location = "South India"
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "RG" {
-  name                = "TerrafromAppServicePlan"
-  location            = azurerm_resource_group.RG.location
-  resource_group_name = azurerm_resource_group.RG.name
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   sku {
     tier = "Standard"
@@ -22,18 +23,18 @@ resource "azurerm_app_service_plan" "RG" {
   }
 }
 
-resource "azurerm_app_service" "RG" {
-  name                = "TerraformAppService"
-  location            = azurerm_resource_group.RG.location
-  resource_group_name = azurerm_resource_group.RG.name
-  app_service_plan_id = azurerm_app_service_plan.RG.id
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
 
-   app_settings = {
+  app_settings = {
     "SOME_KEY" = "some-value"
   }
 
@@ -42,5 +43,4 @@ resource "azurerm_app_service" "RG" {
     type  = "SQLServer"
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
-
-} 
+}
